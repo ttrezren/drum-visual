62bc87663aa36f9ace362799768054ffaa3a7e50
Tim Renner <pdxbass@gmail.com>
2019-07-13 09:39:41 -0700

    stripped to basics

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-        <h1>Circle pack in d3</h1>
+        <h1>Visualizing data with d3</h1>
         <h2>{{ msg }}</h2>
         <svg id="viz"
             :height='height'
             :width='width'
         >
             <g transform="translate(50,50)">
+            <!--"output" is a computed function-->
                 <circle
                     v-for="c in output"
                     :key="c.id"
@@ -29,72 +30,33 @@ export default {
     name: "Graph",
     data() {
         return {
-            msg: "👏 from the Graph component",
             height: 600,
-            width: 600,
-            tweetData: {
-                tweets: [] //array of tweets;
-            },
-        };
+            width: 600
+        }
     },
     created() {
-        this.colourScale = d3
-            .scaleOrdinal()
-            .range(["#5EAFC6","#FE9922","#93C464","#75739F"]);
     },
     mounted() {
         this.getData();
     },
     methods: {
-        packChart() {
-            console.log('packChart invoked')
-            const packChart = d3.pack();
-            packChart.size([500,500]);
-            packChart.padding(10);
-            const output = packChart(this.packData).descendants();
-            console.log("output is", output)
-            return output.map((d,i) => {
-                const fill = this.colourScale(d.depth);
-                return {
-                    id: i + 1,
-                    r: d.r,
-                    x: d.x,
-                    y: d.y,
-                    fill,
-                    stroke: "grey"
-                };
-            });
+        formatData(){
         },
         getData() {
             var that = this;
-            d3.json('tweets.json', function(data){
-                that.tweetData = data;
-                console.log(that.tweetData);
+            d3.json('data.json', function(data){
+                that.componentData = data;
+                console.log(that.componentData);
             });
         }
     },
     computed: {
-        packData() {
-            console.log('packData invoked on ' , this.tweetData)
-            const nestedTweets = d3
-                .nest()
-                .key(d => d.user)
-                .entries(this.tweetData.tweets);
-        
-            const packableTweets = {
-                id: "All Tweets",
-                values: nestedTweets
-                };
-            console.log('packable ',packableTweets)
-            return d3
-                .hierarchy(packableTweets, d => d.values)
-                .sum(d => d.retweets ? d.retweets.length + d.favorites.length + 1 : 1 )
-        },
         output() {
-            return this.packChart();
+            return this.formatData();
         }
     }
-}</script>
+}
+</script>
 
 <style>
 #viz {
